25c52134d027f1d1a0e4e35eddbcdc74527340e6
sonofglassperson <ntshabalalait@gmail.com>
2024-11-14 16:36:56 +0200

    page load events

---
+++ page_load_events/style.js
@@ -0,0 +1,19 @@
+
+const child = document.querySelector('.child')
+const parent = document.querySelector('.parent')
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('dom content loaded!');
+})
+
+document.addEventListener('load', (event) => {
+    console.log('The page is fully loaded.');
+});
+
+document.addEventListener('beforeUnload', () => {
+    console.log('beforeUnload page event!');
+})
+
+document.addEventListener('unload', () => {
+    console.log('dom content unloaded!');
+})
