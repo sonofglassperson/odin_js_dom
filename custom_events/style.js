26770d91f763f454172dd8ab0400d96ac037c70e
sonofglassperson <ntshabalalait@gmail.com>
2024-11-23 14:14:44 +0200

    update custom event code

--- custom_events/style.js
+++ custom_events/style.js
@@ -1,23 +1,18 @@
 let number = document.querySelector('#number')
 
-number.addEventListener("changeNumber", (e) => {
-    // e.detail gets data from event object
-    number.innerText = e.detail.number;
-    number.setAttribute('style', `color:${e.detail.color}`)
-});
-
 function changeNumber(n, c) {
-    // listen for custom event
-    // create function
-    // setup detail object with data
-    // dispatch event
-    const cEvent = new CustomEvent('changeNumber', {
-        detail: {
-            number: n,
-            color: c
-        }
-    });
 
-    number.dispatchEvent(cEvent)
+    const numberEvent = new CustomEvent('changeNumber', {
+        detail: {
+            "number": n,
+            "color": c
+        }
+    })
+
+    number.dispatchEvent(numberEvent)
 }
 
+number.addEventListener('changeNumber', (e) => {
+    number.innerText = e.detail.number;
+    number.setAttribute('style', `color:${e.detail.color}`)
+})
